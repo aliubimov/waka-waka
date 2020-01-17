3b92699fc48cccee3ef8271bea3b893e6514cdae
Andrii Liubimov <andrii.liubimov@gmail.com>
2020-01-17 13:20:13 -0500

    Added mouse and some liveness

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #define _DEFAULT_SOURCE
 
 #include <stdlib.h>
+#include <unistd.h>
+
 #include <SDL2/SDL.h>
 #include "lvgl/lvgl.h"
+
 #include "lv_drivers/display/monitor.h"
+#include "lv_drivers/indev/mouse.h"
 
-int main() {
+static lv_disp_buf_t disp_buf1;
+static lv_color_t buf1_1[480*10];
+static lv_indev_drv_t indev_drv;
 
-    lv_init();
+void init_display() {
     monitor_init();
 
-    static lv_disp_buf_t disp_buf1;
-    static lv_color_t buf1_1[480*10];
-
-
     lv_disp_buf_init(&disp_buf1, &buf1_1, NULL, 480*10);
-
 
     lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
@@ -24,18 +25,37 @@ int main() {
     disp_drv.flush_cb = monitor_flush;
 
     lv_disp_drv_register(&disp_drv);
+};
 
+void init_mouse() {
 
-    lv_obj_t *label = lv_label_create(lv_disp_get_scr_act(NULL), NULL);
+    mouse_init();
 
-    lv_label_set_text(label, "hi!!");
+    lv_indev_drv_init(&indev_drv);
+    indev_drv.type = LV_INDEV_TYPE_POINTER;
+    indev_drv.read_cb = mouse_read;
 
+    lv_indev_drv_register(&indev_drv);
+};
+
+int main() {
+
+    lv_init();
+
+    init_display();
+    init_mouse();
+
+    lv_obj_t *label = lv_ta_create(lv_disp_get_scr_act(NULL), NULL);
     lv_obj_t *keyboard = lv_kb_create(lv_disp_get_scr_act(NULL), NULL);
+
+    lv_kb_set_ta(keyboard, label);
 
 
     while(1) {
         lv_task_handler();
-        SDL_Delay(5 * 1000);
+        usleep(5 * 1000);
+
+        lv_tick_inc(5);
     }
 
     return 0;

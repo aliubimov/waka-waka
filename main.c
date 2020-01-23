b4927dae20e0acc300981ccffb9a3dfe9f413ae3
Andrii Liubimov <andrii.liubimov@gmail.com>
2020-01-22 19:04:41 -0500

    Updated style

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include <SDL2/SDL.h>
 #include "lvgl/lvgl.h"
@@ -9,9 +10,14 @@
 #include "lv_drivers/display/monitor.h"
 #include "lv_drivers/indev/mouse.h"
 
+
 static lv_disp_buf_t disp_buf1;
 static lv_color_t buf1_1[480*10];
 static lv_indev_drv_t indev_drv;
+
+
+static lv_style_t page_style;
+
 
 void init_display() {
     monitor_init();
@@ -45,10 +51,39 @@ int main() {
     init_display();
     init_mouse();
 
-    lv_obj_t *label = lv_ta_create(lv_disp_get_scr_act(NULL), NULL);
-    lv_obj_t *keyboard = lv_kb_create(lv_disp_get_scr_act(NULL), NULL);
+//    lv_obj_t *label = lv_ta_create(lv_disp_get_scr_act(NULL), NULL);
+//    lv_obj_t *keyboard = lv_kb_create(lv_disp_get_scr_act(NULL), NULL);
 
-    lv_kb_set_ta(keyboard, label);
+//    lv_kb_set_ta(keyboard, label);
+
+
+    lv_theme_t *th = lv_theme_alien_init(210, NULL);
+    lv_theme_set_current(th);
+
+    lv_style_copy(&page_style, th->style.bg);
+
+    page_style.body.border.width = 0;
+    page_style.body.border.opa= LV_OPA_TRANSP;;
+
+    lv_obj_t *scr = lv_obj_create(NULL, NULL);
+    lv_scr_load(scr);
+
+    lv_obj_t *page = lv_page_create(scr, NULL);
+    lv_page_set_scrl_layout(page, LV_LAYOUT_COL_L);
+    lv_obj_set_style(page, &page_style);
+    lv_obj_set_size(page, lv_obj_get_width(scr), lv_obj_get_height(scr));
+    lv_obj_align(page, NULL, LV_ALIGN_IN_TOP_LEFT, 0, 0);
+
+    for (int i = 0; i < 100; i++) 
+    {
+        char str[3];
+        sprintf(str, "%d",i);
+
+        lv_obj_t *lbl = lv_label_create(page, NULL);
+        lv_obj_set_width(lbl, lv_page_get_fit_width(page));
+
+        lv_label_set_text(lbl, str);
+    };
 
 
     while(1) {

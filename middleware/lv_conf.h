8dbb231a59b0e7144eab45bb90b6a9cb5a696127
Andrii Liubimov <andrii.liubimov@gmail.com>
2020-02-01 20:38:59 -0500

    Remove fonts

--- middleware/lv_conf.h
+++ middleware/lv_conf.h
@@ -302,10 +302,10 @@ typedef void * lv_indev_drv_user_data_t;            /*Type of user data in the i
 
 /* Robot fonts with bpp = 4
  * https://fonts.google.com/specimen/Roboto  */
-#define LV_FONT_ROBOTO_12       1
+#define LV_FONT_ROBOTO_12       0
 #define LV_FONT_ROBOTO_16       1
-#define LV_FONT_ROBOTO_22       1
-#define LV_FONT_ROBOTO_28       1
+#define LV_FONT_ROBOTO_22       0
+#define LV_FONT_ROBOTO_28       0
 
 /* Demonstrate special features */
 #define LV_FONT_ROBOTO_12_SUBPX 1
